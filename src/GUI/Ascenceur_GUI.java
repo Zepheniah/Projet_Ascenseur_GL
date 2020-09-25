d39c6f5bdc1709d109a1e1be49afa1dff77819e8
binaire <alexbwery@mail.com>
2020-09-25 17:45:48 +0200

    make ihm

--- src/GUI/Ascenceur_GUI.java
+++ src/GUI/Ascenceur_GUI.java
@@ -1,18 +1,39 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Ascenceur_GUI {
-    private JButton a3Button;
-    private JButton a2Button;
+    private JTextField currentPosition;
+    private JTextField state;
+    private JSpinner floor;
+    private JButton ValidStageButton;
     private JButton ARRETURGENCEButton;
-    private JButton RDCButton;
-    private JButton a1Button;
     private JPanel gui;
 
+    public Ascenceur_GUI(int minFloor, int maxFloor) {
+        gui = new JPanel();
+        gui.setLayout(new GridLayout(4, 2));
+
+        gui.add(new Label("Current Position"));
+        gui.add(currentPosition = new JTextField());
+
+        gui.add(new Label("State"));
+        gui.add(state = new JTextField());
+
+        JPanel command = new JPanel();
+        command.setLayout(new FlowLayout());
+        command.add(floor = new JSpinner(new SpinnerNumberModel(0, 0, 509, 1)));
+        command.add(ValidStageButton = new JButton("OK"));
+        gui.add(new Label("Choice floor"));
+        gui.add(command);
+        gui.add(new Label());
+        gui.add(ARRETURGENCEButton = new JButton("Force STOP"));
+    }
     public static void main(String[] args) {
         JFrame frame = new JFrame("Ascenceur_GUI");
-        frame.setContentPane(new Ascenceur_GUI().gui);
+        Ascenceur_GUI gui = new Ascenceur_GUI(0, 3);
+        frame.setContentPane(gui.gui);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);

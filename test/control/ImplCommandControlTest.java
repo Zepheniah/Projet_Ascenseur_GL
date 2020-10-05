f1ed4b49bb1f3e12b21e2373f03b194175a3bb34
Alexandre Wery <alexbwery@gmail.com>
2020-10-05 15:29:35 +0200

    simulation test with one and two command

--- test/control/ImplCommandControlTest.java
+++ test/control/ImplCommandControlTest.java
@@ -1,10 +1,15 @@
 package control;
 
+import algorithme.NearestFloor;
+import control.algorithm.Sort;
 import control.command.Direction;
+import control.command.FloorRequest;
 import org.junit.jupiter.api.Test;
 
+import java.util.TreeSet;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 class ImplCommandControlTest {
     @Test
@@ -31,16 +36,54 @@ class ImplCommandControlTest {
         for (int i = -20; i <= 20; i++)
             assertEquals(Direction.NONE, CommandControl.floorDirection(i, i));
     }
+
     @Test
     void test_nextStop() {
         for (int i = -20; i <= 20; i++) {
-            assertTrue(CommandControl.isNextStop(i, i+1));
-            assertTrue(CommandControl.isNextStop(i+1, i));
+            assertTrue(CommandControl.isNextStop(i, i + 1));
+            assertTrue(CommandControl.isNextStop(i + 1, i));
         }
         for (int i = -20; i <= 20; i++)
             for (int j = i + 2; j <= 20; j++) {
-                assertFalse(CommandControl.isNextStop(i,j));
-                assertFalse(CommandControl.isNextStop(j,i));
+                assertFalse(CommandControl.isNextStop(i, j));
+                assertFalse(CommandControl.isNextStop(j, i));
             }
+    }
+
+    @Test
+    void test_simulation_one_command() {
+        OperationalCommand operationalCommand = mock(OperationalCommand.class);
+        ImplCommandControl control = new ImplCommandControl(0, operationalCommand);
+        control.setCommands(new TreeSet<>(new Sort()));
+        control.addFloorRequest(new FloorRequest(Direction.UP, 10));
+        verify(operationalCommand, times(1)).up();
+        for (int i = 1; i < 9; i++) {
+            control.reachFloor(i);
+            verify(operationalCommand, never()).stopNextFloor();
+        }
+        control.reachFloor(9);
+        verify(operationalCommand, never()).down();
+        verify(operationalCommand, never()).emergencyBreak();
+        verify(operationalCommand, times(1)).stopNextFloor();
+    }
+
+    @Test
+    void test_simulation_two_command() {
+        OperationalCommand operationalCommand = mock(OperationalCommand.class);
+        ImplCommandControl control = new ImplCommandControl(0, operationalCommand);
+        control.setCommands(new TreeSet<>(new Sort()));
+        control.addFloorRequest(new FloorRequest(Direction.UP, -10));
+        control.addFloorRequest(new FloorRequest(Direction.UP, 10));
+        verify(operationalCommand, times(1)).down();
+        verify(operationalCommand, never()).up();
+        for (int i = 1; i < 9; i++) {
+            control.reachFloor(-i);
+            verify(operationalCommand, never()).stopNextFloor();
+        }
+        control.reachFloor(-9);
+        verify(operationalCommand, times(1)).down();
+        verify(operationalCommand, times(1)).up();
+        verify(operationalCommand, never()).emergencyBreak();
+        verify(operationalCommand, times(1)).stopNextFloor();
     }
 }
